ee23f904c42fb70fbdf3ff2818d3536a05ea0f30
aetherdash <101219368+aetherdash@users.noreply.github.com>
2022-06-01 13:50:33 -0700

    Update and rename test.txt to ecs-rollback.sh

---
+++ scripts/ecs-rollback.sh
@@ -0,0 +1,3 @@
+aws ecr batch-delete-image --repository-name $environment --image-ids imageTag=$tag
+MY_MANIFEST=$(aws ecr batch-get-image --repository-name $environment --image-ids imageTag=$hash --region us-east-1 --query images[].imageManifest --output text)
+aws ecr put-image --repository-name $environment --image-tag $tag --image-manifest "$MY_MANIFEST" --region us-east-1
