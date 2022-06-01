d02e1a22f39d6e6bc25cd3e12b5c187bab36ad52
aetherdash <101219368+aetherdash@users.noreply.github.com>
2022-06-01 14:34:05 -0700

    Update ecs-rollback.sh

--- scripts/ecs-rollback.sh
+++ scripts/ecs-rollback.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 aws configure set aws_access_key_id $AWS_ACCESS_KEY_ID
 aws configure set aws_secret_access_key $AWS_SECRET_ACCESS_KEY
-aws ecr batch-delete-image --repository-name $environment --image-ids imageTag=$tag
-MY_MANIFEST=$(aws ecr batch-get-image --repository-name $environment --image-ids imageTag=$hash --region us-east-1 --query images[].imageManifest --output text)
-aws ecr put-image --repository-name $environment --image-tag $tag --image-manifest "$MY_MANIFEST" --region us-east-1
+aws ecr batch-delete-image --repository-name graphql/$environment --image-ids imageTag=$tag
+MY_MANIFEST=$(aws ecr batch-get-image --repository-name graphql/$environment --image-ids imageTag=$hash --region us-east-1 --query images[].imageManifest --output text)
+aws ecr put-image --repository-name graphql/$environment --image-tag $tag --image-manifest "$MY_MANIFEST" --region us-east-1
